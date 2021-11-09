0a651e0e0575480f11143195807ff32cef6cf95a
wgyYOYO <2543054794@qq.com>
2021-11-09 16:50:46 +0800

    格式化实体类，以及创建数据源模板

--- src/main/java/wgy/entity/User.java
+++ src/main/java/wgy/entity/User.java
@@ -4,11 +4,14 @@ import lombok.Data;
 
 @Data
 public class User {
-    String name;
-    String year;
-    String day;
-    String sex;
-    String month;
-    String date;
-    String stuId;
+    String name;//姓名
+    String sex;//性别
+    String stuId;//学号
+    String department;//院系
+    String perfessional;//专业
+    String birthYear;//出生年
+    String birthMonth;//出生月
+    String birthDay;//出生日
+    String validityYear;//有效年
+    String domitory;//宿舍
 }
